c9dc3911ec9c093fc3abb4d88feae039da44f52a
Quan <quanp155@gmail.com>
2021-03-05 16:15:27 -0600

    Adjusted module code for gameboard

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const gameBoard = {
-    boardArray: [],
-    otherProperty: 10,
-    "randomFunction": function() {
-        
+const gameBoard = (() => {
+    const board = [];
+    //allows outside modules to pull the board array
+    const getBoard = () => {
+        return board;
     }
-}
+})();
 
 const players = {
+    //cells selected by the players
     player1Selections: [],
     player2Selections: [],
-    
+    //tracking points, could possibly be done with a different counter
+    points1: [],
+    points2: []
 }
