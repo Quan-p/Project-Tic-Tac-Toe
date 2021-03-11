f08b4ae1ca06e6b82b8cad082578ae82aec0c6ad
Quan <quanp155@gmail.com>
2021-03-11 16:41:36 -0600

    Adjusted code to detect a single click per cell

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
 const gameBoard = (() => {
-    const board = [];
-    const showBoard = () => {
-        document.getElementById('game-container').style.display = "grid";
-    }
+    //const board = [];
+    // const showBoard = () => {
+    //     document.getElementById('game-container').style.display = "grid";
+    // }
 
-    const cellElements = document.querySelectorAll('[data-cell]')
+    //allows outside modules to pull the board array
+   
+
+    return {
+        //showBoard,
+    };
+})();
+
+const placeMark = (() => {
+    const cellElements = document.querySelectorAll('[data-cell]');
     cellElements.forEach(cell => {
-        cellElements.addEventListener('click', handleClick, {once: true})
+    cell.addEventListener('click', handleClick, {once: true})
     })
 
+    const X_CLASS = 'x';
+    const CIRCLE_CLASS ='circle';
+    
     function handleClick(e) {
-        console.log('clicked')
-    }
-    //allows outside modules to pull the board array
-    const getBoard = () => {
-        return board;
+        console.log('clicked');
+        // const cell = e.target;
+        // const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+        // placeMark(cell, currentClass)
     }
 
     return {
         handleClick,
-        getBoard,
-        showBoard,
-    };
-})();
+
+    }
+})(); 
+
+
 //gameBoard.showBoard()
-const players = {
-    //cells selected by the players
-    player1Selections: [],
-    player2Selections: [],
-    //tracking points, could possibly be done with a different counter
-    points1: [],
-    points2: []
-}
+// const players = {
+//     //cells selected by the players
+//     player1Selections: [],
+//     player2Selections: [],
+//     //tracking points, could possibly be done with a different counter
+//     points1: [],
+//     points2: []
+
